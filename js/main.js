d482f0af2f8001dfd3c2f5d1ae4fbfd7bc269869
Timur <sol485@outlook.com>
2019-11-13 20:33:30 +0300

    Module3-task1 done

---
+++ js/main.js
@@ -0,0 +1,126 @@
+'use strict';
+
+// КОНСТАНТЫ //
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+
+// ПЕРЕМЕННЫЕ //
+
+var postsArray = [];
+var fragment = document.createDocumentFragment();
+var pictureTemplate = document.querySelector("#picture")
+  .content
+  .querySelector(".picture");
+var pictures = document.querySelector(".pictures");
+var bigPicture = document.querySelector(".big-picture");
+var photosNumArray = getNumbersArray(25);
+
+// ФУНКЦИИ //
+
+function getRandomElement (arr) {
+  var min = 0;
+  var max = arr.length - 1;
+  var randomElement = Math.floor(Math.random() * (max + 1));
+  return arr[randomElement];
+}
+
+function getRandomNumber (min, max) {
+  var minNum = Math.ceil(min);
+  var maxNum = Math.floor(max);
+  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum; //The maximum is inclusive and the minimum is inclusive
+}
+
+function getNumbersArray (num) {
+  var arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(i + 1);
+  }
+  return arr;
+}
+
+function createComment () {
+  var comment = getRandomElement(COMMENTS);
+  if (Math.round(Math.random()) == true) {
+    comment += ' ' + getRandomElement(COMMENTS);
+  }
+  return comment;
+}
+
+function createPost () {
+  var post = {};
+  post.url = null;
+  post.likes = getRandomNumber(15, 200);
+  post.comments = [];
+  for (let i = 0; i < getRandomNumber(5, 20); i++) {
+    post.comments.push(createComment());
+  }
+  return post;
+}
+
+function renderPost (post) {
+  var renderedPost = pictureTemplate.cloneNode(true);
+
+  renderedPost.querySelector(".picture__img").setAttribute('src', post.url);
+  renderedPost.querySelector(".picture__likes").textContent = post.likes;
+  renderedPost.querySelector(".picture__comments").textContent = post.comments;
+
+  return renderedPost;
+}
+
+function renderSocialComment (comment) {
+  var renderedComment = bigPicture.querySelector(".social__comment").cloneNode(true);
+
+  renderedComment.querySelector(".social__picture").setAttribute("src", "img/avatar-" + getRandomNumber(1, 6) + ".svg");
+  renderedComment.querySelector(".social__text").textContent = comment;
+
+  return renderedComment;
+}
+
+function renderBigPicture (post) {
+  var socialCommentsFragment = document.createDocumentFragment();
+
+  bigPicture.querySelector(".big-picture__img").querySelector("img").setAttribute('src', post.url);
+  bigPicture.querySelector(".likes-count").textContent = post.likes;
+  bigPicture.querySelector(".comments-count").textContent = post.comments.length;
+
+  // Прикрепляем все комментарии к фрагменту //
+  for (var i = 0; i < post.comments.length; i++) {
+    var newComment = renderSocialComment(post.comments[i]);
+    socialCommentsFragment.appendChild(newComment);
+  }
+
+  // Очистим список комментариев //
+  bigPicture.querySelector(".social__comments").innerHTML = "";
+
+  // Добавим комментарии, которые мы сгенерировали //
+  bigPicture.querySelector(".social__comments").appendChild(socialCommentsFragment);
+}
+
+
+// ВЫЗОВ ФУНКЦИЙ И ВЫПОЛНЯЕМЫЙ КОД //
+
+
+for (let i = 0; i < 25; i++) {
+  postsArray.push(createPost());
+  postsArray[i].url = "photos/" + (i + 1) + ".jpg";
+}
+
+for (var i = 0; i < postsArray.length; i++) {
+  fragment.appendChild(renderPost(postsArray[i]));
+}
+
+pictures.appendChild(fragment);
+
+renderBigPicture(postsArray[0]);
+
+document.querySelector(".big-picture").classList.remove("hidden");
+
+document.querySelector(".social__comment-count").classList.add("visually-hidden");
+document.querySelector(".comments__loader").classList.add("visually-hidden");
