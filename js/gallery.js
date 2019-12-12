291e2d70eb4cc63c28618dd7210347f060f7540e
Timur <sol485@outlook.com>
2019-12-12 21:25:04 +0300

    Module7-task1 done

--- js/gallery.js
+++ js/gallery.js
@@ -2,72 +2,109 @@
 
 (function () {
 
+  // Возможно реализовать:
+  //
+  // 2. Поиск внутри postsArray по совпадению с src="photo..." кликнутого фото (возможно создать мапу или словарь)
+  // 3. Устранить дребезг
 
   let postsArray = [];
-  const pictureTemplate = document.querySelector("#picture")
-    .content
-    .querySelector(".picture");
-  const pictures = document.querySelector(".pictures");
-  const picturesArray = document.querySelectorAll(".picture");
+  let postsArrayCopy = [];
+
   const bigPicture = document.querySelector(".big-picture");
-
-  function createComment () {
-    var comment = window.data.getRandomElement(COMMENTS);
-    if (Math.round(Math.random()) == true) {
-      comment += ' ' + window.data.getRandomElement(COMMENTS);
-    }
-    return comment;
-  }
-
-  function createPost () {
-    let post = {};
-    post.url = null;
-    post.likes = window.data.getRandomNumber(15, 200);
-    post.comments = [];
-    for (let i = 0; i < window.data.getRandomNumber(5, 20); i++) {
-      post.comments.push(createComment());
-    }
-    return post;
-  }
+  const cancelPreview = document.querySelector("#picture-cancel");
 
   function renderPost (post) {
+    const pictureTemplate = document.querySelector("#picture")
+      .content
+      .querySelector(".picture");
     const renderedPost = pictureTemplate.cloneNode(true);
 
     renderedPost.querySelector(".picture__img").setAttribute('src', post.url);
     renderedPost.querySelector(".picture__likes").textContent = post.likes;
     renderedPost.querySelector(".picture__comments").textContent = post.comments.length;
-    // renderedPost.querySelector(".picture__info").textContent = post.description;
 
     return renderedPost;
   }
 
-  function renderSocialComment (comment) {
-    const renderedComment = bigPicture.querySelector(".social__comment").cloneNode(true);
+  function renderPreviewComment (post) {
+    const comment = document.createElement("li");
+    const pic = document.createElement("img");
+    const text = document.createElement("p");
 
-    renderedComment.querySelector(".social__picture").setAttribute("src", "img/avatar-" + window.data.getRandomNumber(1, 6) + ".svg");
-    renderedComment.querySelector(".social__text").textContent = comment;
+    comment.className = "social__comment";
+    pic.className = "social__picture";
+    text.className = "social__text";
+    comment.appendChild(pic);
+    comment.appendChild(text);
 
-    return renderedComment;
+    comment.querySelector(".social__picture").setAttribute("src", post.avatar);
+    comment.querySelector(".social__picture").setAttribute("width", 35);
+    comment.querySelector(".social__picture").setAttribute("height", 35);
+    comment.querySelector(".social__text").textContent = post.message;
+
+    return comment;
   }
 
-  function renderBigPicture (post) {
+  function renderPreview (post) {
     const socialCommentsFragment = document.createDocumentFragment();
+    console.log('renderPreview post', post);
+    const img = document.createElement("img");
 
+    bigPicture.querySelector(".big-picture__img").appendChild(img);
     bigPicture.querySelector(".big-picture__img").querySelector("img").setAttribute('src', post.url);
     bigPicture.querySelector(".likes-count").textContent = post.likes;
     bigPicture.querySelector(".comments-count").textContent = post.comments.length;
+    bigPicture.querySelector(".social__caption").textContent = post.description;
 
-    // Прикрепляем все комментарии к фрагменту //
+    // Прикрепляем все комментарии к фрагменту
     for (let i = 0; i < post.comments.length; i++) {
-      const newComment = renderSocialComment(post.comments[i]);
+      const newComment = renderPreviewComment(post.comments[i]);
       socialCommentsFragment.appendChild(newComment);
     }
-
-    // Очистим список комментариев //
+    // Очистим список комментариев
     bigPicture.querySelector(".social__comments").innerHTML = "";
-
-    // Добавим комментарии, которые мы сгенерировали //
+    // Добавим комментарии, которые мы сгенерировали
     bigPicture.querySelector(".social__comments").appendChild(socialCommentsFragment);
+    // Скрываем все комментарии, кроме первых 5
+    const comments = bigPicture.querySelectorAll(".social__comment");
+    console.log("comments", comments);
+    if (comments.length > 5) {
+      for (let i = 5; i < comments.length; i++) {
+        comments[i].className += " visually-hidden";
+      }
+    }
+    let hidden = bigPicture.querySelectorAll("li.visually-hidden");
+    console.log("hidden", hidden);
+    // Вешаем слушатель на comment-loader для загрузки комментариев
+    const loader = bigPicture.querySelector(".comments-loader");
+    // Покажем загрузчик, если он скрыт
+    if (loader.classList.contains("visually-hidden")) loader.classList.remove("visually-hidden");
+
+    // 1. Найти и задать правильно число изначально открытых комментариев
+    // 2. Обновить число комментариев после нажатия на кнопку
+    // 3. Правильно задать число, если при последнем нажатии было меньше 5 комментариев
+
+    loader.addEventListener( "click", (evt) => {
+      const commentCount = bigPicture.querySelector(".social__comment-count");
+      const notHidden = bigPicture.querySelectorAll(".social__comment:not(.visually-hidden)");
+      console.log("notHidden inside loader", notHidden);
+      // Проверка, есть ли ещё спрятанные комментарии, если нет, то скрыть "загрузчик"
+      if (hidden.length >= 5) {
+        for (let i = 0; i < 5; i++) {
+          if (hidden[i].classList.contains("visually-hidden")) hidden[i].classList.remove("visually-hidden");
+        }
+        commentCount.innerHTML = (5 + comments.length - hidden.length) + " из " + "<span class='comments-count'>" + post.comments.length + "</span>" + " комментариев";
+      } else if (hidden.length < 5) {
+        for (let i = 0; i < hidden.length; i++) {
+          if (hidden[i].classList.contains("visually-hidden")) hidden[i].classList.remove("visually-hidden");
+        }
+        hidden = bigPicture.querySelectorAll("li.visually-hidden");
+        commentCount.innerHTML = (comments.length - hidden.length) + " из " + "<span class='comments-count'>" + post.comments.length + "</span>" + " комментариев";
+      }
+      if (hidden.length === 0 ) loader.classList.add("visually-hidden");
+      hidden = bigPicture.querySelectorAll("li.visually-hidden");
+      console.log("hidden inside loade", hidden)
+    });
   }
 
   const closePreview = () => {
@@ -86,23 +123,69 @@
     document.addEventListener("keydown", onPreviewEscPress);
   };
 
-  let renderPosts = (posts) => {
+  const renderPosts = (posts) => {
+    const pictures = document.querySelector(".pictures");
+
     let fragment = document.createDocumentFragment();
     for (let i = 0; i < posts.length; i++) {
       fragment.appendChild(renderPost(posts[i]));
     }
 
     pictures.appendChild(fragment);
-    renderBigPicture(posts[0]);
+
+    const picturesArray = document.querySelectorAll(".picture");
+
+    picturesArray.forEach( (elem) => {
+      elem.addEventListener("click", (evt) => {
+        clearPreview();
+        let photo = evt.target.src.slice(32);
+        let post = new Object();
+        postsArray.forEach( (el) => {
+          if (el.url === photo) {
+            post = el;
+          }
+        })
+        renderPreview(post);
+        openPreview();
+      });
+    });
   }
 
-  let clearPosts = () => {
-    pictures.innerHTML = "";
+  const clearPosts = () => {
+    const pics = document.querySelectorAll(".picture");
+    pics.forEach( (el) => {
+      el.remove();
+    });
+  }
+
+  const clearPreview = () => {
+    const picture = bigPicture.querySelector(".big-picture__img");
+    const socialHeader = bigPicture.querySelector(".social__header");
+    const caption = bigPicture.querySelector(".social__caption");
+    const likes = bigPicture.querySelector(".likes-count");
+    const commentsCount = bigPicture.querySelector(".comments-count");
+    const comments = bigPicture.querySelector(".social__comments")
+
+    picture.innerHTML = "";
+    caption.textContent = "";
+    likes.textContent = "";
+    commentsCount.textContent = "";
+    comments.innerHTML = "";
+  }
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function shuffleTwo(array) {
+    array.sort(() => Math.random() - 0.5);
   }
 
   // Добавим локальный successHandler, чтобы сохранить загруженный массив
   // в исходном состоянии
-  let postsArrayCopy = [];
 
   let successHandler = (data) => {
     postsArray = data;
@@ -113,48 +196,58 @@
     // После загрузки данных показать вкладки на странице
     const imgFilters = document.querySelector(".img-filters");
     imgFilters.classList.remove("img-filters--inactive");
+
+    const imgFiltersBtns = document.querySelectorAll(".img-filters__button");
+    const imgFilterPopular = document.querySelector("#filter-popular");
+    const imgFilterNew = document.querySelector("#filter-new");
+    const imgFilterDiscussed = document.querySelector("#filter-discussed");
+
+
+    imgFiltersBtns.forEach( (elem) => {
+      elem.addEventListener("click", (evt) => {
+        if (evt.target.classList.contains("img-filters__button--active")) return;
+
+          imgFiltersBtns.forEach( (el) => {
+            el.classList.remove("img-filters__button--active");
+          });
+
+          evt.target.classList.add("img-filters__button--active");
+
+        window.debounce( () => {
+          switch (evt.target.id) {
+            case "filter-discussed":
+              clearPosts();
+              postsArrayCopy.sort((left, right) => {
+                return right.comments.length - left.comments.length;
+              });
+              renderPosts(postsArrayCopy);
+            break;
+            case "filter-new":
+              clearPosts();
+              shuffle(postsArrayCopy);
+              renderPosts(postsArrayCopy);
+            break;
+            case "filter-popular":
+              clearPosts();
+              renderPosts(postsArray);
+            break;
+          }
+        });
+      });
+    });
   }
+
+  cancelPreview.addEventListener("click", (evt) => {
+    closePreview();
+  })
 
   // Загрузка данных
   window.back.downloadData(successHandler, window.back.errorHandler);
 
-  document.querySelector(".social__comment-count").classList.add("visually-hidden");
-  // document.querySelector(".comments-loader").classList.add("visually-hidden");
-
-  // Добавление всем элементам с классом .picture открытие по клику
-  picturesArray.forEach( (elem) => {
-    elem.addEventListener("click", (evt) => {
-      openPreview();
-    })
-  });
-
   // module7-task1
   // Добавим переключение состояния кнопкам сортировки и
   // повесим на них события по сортировке и рендеру массива фотографий
-  const imgFiltersBtns = document.querySelectorAll(".img-filters__button");
-  const imgFilterPopular = document.querySelector("#filter-popular");
-  const imgFilterNew = document.querySelector("#filter-new");
-  const imgFilterDiscussed = document.querySelector("#filter-discussed");
 
-  // imgFiltersBtns.forEach( (elem) => {
-  //   elem.addEventListener("click", (evt) => {
-  //     if (evt.target.classList.contains("img-filters__button--active")) return;
-  //     imgFiltersBtns.forEach( (el) => {
-  //       el.classList.remove("img-filters__button--active");
-  //     })
-  //     evt.target.classList.add("img-filters__button--active");
-  //     switch (evt.target.id) {
-  //       case "filter-popular":
-  //         postsArrayCopy.sort((left, right) => {
-  //           return right.comments.length - left.comments.length;
-  //        });
-  //         break;
-  //       case "filter-new":
-  //         postsArrayCopy
-
-  //     }
-  //   })
-  // });
 
   //Сортируем массив
 
